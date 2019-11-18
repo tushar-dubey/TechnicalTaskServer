cc6a8a8dbc3434f6ea4d8c0a08c82b83be4a47fa
tushar <tdiitg@gmail.com>
2019-11-18 18:05:20 +0530

    trying to debug protoc not in path

--- install_protoc.sh
+++ install_protoc.sh
@@ -2,10 +2,8 @@
 
 PROTOBUF_VERSION=3.3.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd /home/travis
-wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
-unzip ${PROTOC_FILENAME}
-bin/protoc --version
-chmod +x bin/protoc
-popd
-PATH="/home/travis/bin:${PATH}"
+  - go get -v -d google.golang.org/grpc
+  - go get -v -d -t github.com/golang/protobuf/...
+  - curl -L https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip -o /tmp/protoc.zip
+  - unzip /tmp/protoc.zip -d "$HOME"/protoc
+  - mkdir -p "$HOME"/src && ln -s "$HOME"/protoc "$HOME"/src/protobuf
